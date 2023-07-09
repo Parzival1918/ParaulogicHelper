559964235ab15beda6f2fd84564e19c61019cce9
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-07-09 08:57:01 +0200

    feat: Delete unnecessary args

--- paraulogic_game.py
+++ paraulogic_game.py
@@ -7,28 +7,18 @@ import argparse as ap
 
 #Local imports
 from gameHelpers import today
-from gameHelpers import date
 from gameHelpers import beautiful_print as bp
 
 import importlib
 importlib.reload(today)
 importlib.reload(bp)
-importlib.reload(date)
 
 parser = ap.ArgumentParser()
 
-parser.add_argument('-d', '--date', type=str, help='Date of the game in format YYYY-MM-DD')
-parser.add_argument('-t', '--today', help='Play today\'s game', action='store_true')
+# parser.add_argument('-t', '--today', help='Play today\'s game', action='store_true')
 parser.add_argument('-j', '--json-output', help='Output the game data as json', action='store_true')
 
 args = parser.parse_args()
 
-if args.today:
-    bp.print_word('Today\'s game: ', bp.TextColour.YELLOW, end='\n')
-    today.play_today()
-elif args.date:
-    bp.print_word('Unsupported function "-d/--date"', bp.TextColour.BOLD + bp.TextColour.RED, end='\n')
-    exit(0)
-    # bp.print_word('Game of ', bp.TextColour.YELLOW)
-    # bp.print_word(args.date, bp.TextColour.BOLD + bp.TextColour.BLUE, end=':\n')
-    # date.play_date(args.date)
+bp.print_word('Today\'s game: ', bp.TextColour.YELLOW, end='\n')
+today.play_today()
